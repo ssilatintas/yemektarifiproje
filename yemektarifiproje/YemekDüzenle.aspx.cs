ca54d6627e8c62154c06cd2eebcc73d6b5240651
lenovo <silatintas1@gmail.com>
2024-04-07 21:12:30 +0300

    yemek tarifi 36.video

--- yemektarifiproje/YemekDüzenle.aspx.cs
+++ yemektarifiproje/YemekDüzenle.aspx.cs
@@ -26,11 +26,30 @@ namespace yemektarifiproje
                     TextBox3.Text = dr[3].ToString();
                 }
                 bgl.baglanti().Close();
+                if (Page.IsPostBack == false)
+                {
+                    //Kategori Listesi
+                    SqlCommand komut2 = new SqlCommand("Select*From Tbl_Kategoriler", bgl.baglanti());
+                    SqlDataReader dr2 = komut2.ExecuteReader();
+                    DropDownList1.DataTextField = "KateoriAd";
+                    DropDownList1.DataValueField = "Kategoriid";
+
+                    DropDownList1.DataSource = dr2;
+                    DropDownList1.DataBind();
+                }
             }
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            SqlCommand komut = new SqlCommand("update Tbl_Yemekler set YemekAd=@p1,YemekMalzeme=@p2,YemekTarif=@p3,Kategoriid=@p4 where Yemekid=@p5", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", TextBox1.Text);
+            komut.Parameters.AddWithValue("@p2", TextBox2.Text);
+            komut.Parameters.AddWithValue("@p3", TextBox3.Text);
+            komut.Parameters.AddWithValue("@p4", DropDownList1.SelectedValue);
+            komut.Parameters.AddWithValue("@p5", id);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
 
         }
     }
